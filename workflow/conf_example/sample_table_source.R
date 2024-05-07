b56e4bf197572523f594bdfafc75969f5373df06
maxfieldkelsey <maxfield_kelsey@brown.edu>
2024-05-07 15:51:25 -0400

    changes to read me and conf ex

--- workflow/conf_example/sample_table_source.R
+++ workflow/conf_example/sample_table_source.R
@@ -4,4 +4,4 @@ sample_table <- read_csv(conf[["sample_table"]])
 sample_table <- sample_table[match(conf$samples, sample_table$sample_name), ]
 #set any factors here
 #e.g. sample_table <- sample_table %>% mutate(braak = as.factor(braak, levels = c("O", "I", "II", "III", "IV", "V","V/VI", "VI")))
-sample_table <- sample_table %>% mutate(braak = factor(braak, levels = c("O", "I", "II", "III", "IV", "V","V/VI", "VI")))
+# sample_table <- sample_table %>% mutate(braak = factor(braak, levels = c("O", "I", "II", "III", "IV", "V","V/VI", "VI")))
