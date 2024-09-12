a2b1c573fb319f25107e12559308c60dfada3786
maxfieldkelsey <maxfield_kelsey@brown.edu>
2024-09-12 16:26:58 -0400

    updated colors

--- workflow/scripts/generate_colors_to_source.R
+++ workflow/scripts/generate_colors_to_source.R
@@ -64,7 +64,7 @@ if (length(conf$levels) == 1) {
     )
 
     {
-        scale_palette <- list(paletteer::scale_fill_paletteer_d(conf$default_palette), paletteer::scale_color_paletteer_d(conf$default_palette))
+        scale_palette <- list(scale_fill_viridis_d(), scale_color_viridis_d())
         scale_palette_alt <- list(paletteer::scale_fill_paletteer_d("ggthemes::few_Dark"), paletteer::scale_color_paletteer_d("ggthemes::few_Dark"))
 
         scale_samples_unique <- list(scale_fill_manual(values = sample_unique_palette), scale_color_manual(values = sample_unique_palette))
@@ -132,7 +132,7 @@ if (length(conf$levels) == 1) {
     )
 
     {
-        scale_palette <- list(paletteer::scale_fill_paletteer_d(conf$default_palette), paletteer::scale_color_paletteer_d(conf$default_palette))
+        scale_palette <- list(scale_fill_viridis_d(), scale_color_viridis_d())
         scale_palette_alt <- list(paletteer::scale_fill_paletteer_d("ggthemes::few_Dark"), paletteer::scale_color_paletteer_d("ggthemes::few_Dark"))
 
         scale_samples_unique <- list(scale_fill_manual(values = sample_unique_palette), scale_color_manual(values = sample_unique_palette))
@@ -193,9 +193,8 @@ if (length(conf$levels) == 1) {
     )
 
     {
-        scale_palette <- list(paletteer::scale_fill_paletteer_d(conf$default_palette), paletteer::scale_color_paletteer_d(conf$default_palette))
+        scale_palette <- list(scale_fill_viridis_d(), scale_color_viridis_d())
         scale_palette_alt <- list(paletteer::scale_fill_paletteer_d("ggthemes::few_Dark"), paletteer::scale_color_paletteer_d("ggthemes::few_Dark"))
-
         scale_samples_unique <- list(scale_fill_manual(values = sample_unique_palette), scale_color_manual(values = sample_unique_palette))
         scale_samples <- list(scale_fill_manual(values = sample_palette), scale_color_manual(values = sample_palette))
         scale_conditions <- list(scale_fill_manual(values = condition_palette), scale_color_manual(values = condition_palette))
